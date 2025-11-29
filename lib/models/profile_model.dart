28414dc6ed98b445f82e8530a06fecd87dd8a071
Antonio0203 <carvalhos.tor07@gmail.com>
2025-11-28 21:50:55 -0300

    Criei o arquivo de perfil para models e controllers. Criei a estrutura do código para profiller_model.dart

---
+++ lib/models/profile_model.dart
@@ -0,0 +1,21 @@
+class ProfileModel {
+  final String nome;
+  final String email;
+  final int idade;
+  final String cpf;
+  final String cep;
+  final String endereco;
+  final String numero;
+  final String celular;
+
+  ProfileModel({
+    required this.nome,
+    required this.email,
+    required this.idade,
+    required this.cpf,
+    required this.cep,
+    required this.endereco,
+    required this.numero,
+    required this.celular,
+  });
+}
